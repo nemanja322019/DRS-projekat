ae63118f911b1a393a3c6da8b68177c3e3680657
unknown <nemanjalazarevic0@gmail.com>
2022-12-12 16:55:04 +0100

    Pamcenje ulogovanog korisnika, pristup stranicama

--- Website/auth.py
+++ Website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import *
 from werkzeug .security import generate_password_hash, check_password_hash
-
+from flask_login import login_user,login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,17 +15,20 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash('Uspesno logovanje!',category='success')
+                login_user(user,remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Pogresna lozinka!',category='error')
         else:
             flash('Email ne postoji!',category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
@@ -62,7 +65,8 @@ def sign_up():
             new_user =  User(name=name, last_name=last_name, adress=adress,city=city,country=country,phNumber=phNumber,email=email,password = generate_password_hash(password,method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user,remember=True)
             flash('Uspeno registrovanje',category="success")
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
