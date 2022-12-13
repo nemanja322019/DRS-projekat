9bc6b2211626dc8a9498ae518a8b253f003fa02c
vladimirjosic2000 <vladimirjosic2000@gmail.com>
2022-12-13 14:14:49 +0100

    Ubacio sam klasu CreditCard

--- Website/models.py
+++ Website/models.py
@@ -1,7 +1,12 @@
 from . import db
 from flask_login import UserMixin
 
-
+class CreditCard(db.Model):
+    cardNumber = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(150))
+    date = db.Column(db.String(150))
+    code = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +18,6 @@ class User(db.Model, UserMixin):
     phNumber = db.Column(db.Integer, unique=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+    verified = db.Column(db.Boolean,default= False)
+    creditCard = db.relationship('CreditCard')
+   
